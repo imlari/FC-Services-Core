fa39ae5a90c91c6aa8e91cbb7795823b5a9933db
alisson.assuncao-at-171944429159 <alisson.assuncao@ferreiracosta.com.br>
2023-05-07 10:39:21 -0300

    Ajustando logica com o prototipo com erro

--- CadastrarChamadoClass.cs
+++ CadastrarChamadoClass.cs
@@ -8,6 +8,6 @@ public class Chamado
     public string HorarioUltimaAtualizacao { get; set; }
     public string Status { get; set; }
     public string TempoDecorrido { get; set; }
-""    public int Empregado_Matricula { get; set; }
+    public int Empregado_Matricula { get; set; }
     public string Tipo { get; set; }
 }
