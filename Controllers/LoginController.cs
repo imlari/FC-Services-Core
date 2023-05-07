6163eac946e2ac86ee682dece9952d255e1ca469
alisson.assuncao-at-171944429159 <alisson.assuncao@ferreiracosta.com.br>
2023-05-07 16:44:18 -0300

    Adionando hash na senha

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -6,6 +6,8 @@ using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using System.Security.Cryptography;
+
 
 namespace backend_squad1.Controllers
 {
@@ -25,29 +27,44 @@ namespace backend_squad1.Controllers
             MySqlConnection connection = new MySqlConnection(connectionString);
             MySqlCommand command = connection.CreateCommand();
 
-            command.CommandText = "SELECT COUNT(*) FROM Empregado WHERE Email = @Email AND Senha = @Senha";
+            command.CommandText = "SELECT COUNT(*) FROM Empregado WHERE Email = @Email";
             command.Parameters.AddWithValue("@Email", login.Email);
-            command.Parameters.AddWithValue("@Senha", login.Senha);
             connection.Open();
             int count = Convert.ToInt32(command.ExecuteScalar());
             connection.Close();
 
             if (count == 1)
             {
-                command.CommandText = "SELECT Matricula, Nome FROM Empregado WHERE Email = @Email AND Senha = @Senha";
+                command.CommandText = "SELECT Senha, Matricula, Nome FROM Empregado WHERE Email = @Email";
                 connection.Open();
                 MySqlDataReader reader = command.ExecuteReader();
+                string hashedPassword = null;
                 string matricula = null;
                 string nome = null;
                 while (reader.Read())
                 {
+                    hashedPassword = reader["Senha"].ToString();
                     matricula = reader["Matricula"].ToString();
                     nome = reader["Nome"].ToString();
                 }
                 reader.Close();
                 connection.Close();
 
-                if (!string.IsNullOrEmpty(matricula) && !string.IsNullOrEmpty(nome))
+                // Hashear a senha fornecida pelo usuário
+                string hashedPasswordInput;
+                using (SHA256 sha256Hash = SHA256.Create())
+                {
+                    byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(login.Senha));
+                    StringBuilder builder = new StringBuilder();
+                    for (int i = 0; i < bytes.Length; i++)
+                    {
+                        builder.Append(bytes[i].ToString("x2"));
+                    }
+                    hashedPasswordInput = builder.ToString();
+                }
+
+                // Comparar as duas hashes
+                if (hashedPasswordInput == hashedPassword)
                 {
                     // Gerar o token JWT com as informações do usuário
                     var tokenHandler = new JwtSecurityTokenHandler();
@@ -56,9 +73,9 @@ namespace backend_squad1.Controllers
                     {
                         Subject = new ClaimsIdentity(new Claim[]
                         {
-                            new Claim(ClaimTypes.Name, matricula),
-                            new Claim(ClaimTypes.Email, login.Email),
-                            new Claim("nome", nome)
+                    new Claim(ClaimTypes.Name, matricula),
+                    new Claim(ClaimTypes.Email, login.Email),
+                    new Claim("nome", nome)
                         }),
                         Expires = DateTime.UtcNow.AddDays(7),
                         SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
--- Controllers/RegisterController.cs
+++ Controllers/RegisterController.cs
@@ -1,55 +1,65 @@
 using Microsoft.AspNetCore.Mvc;
 using MySql.Data.MySqlClient;
-namespace backend_squad1.Controllers;
+using System.Security.Cryptography;
+using System.Text;
 
-[ApiController]
-[Route("[controller]")]
-
-
-public class CadastrarUsuarioController : ControllerBase
+namespace backend_squad1.Controllers
 {
-
-
-
-    [HttpPost(Name = "Adicionar Usuario")]
-
-
-    public IActionResult PostCadastrarEmpregado([FromBody] Empregado user)
+    [ApiController]
+    [Route("[controller]")]
+    public class CadastrarUsuarioController : ControllerBase
     {
-
-        string connectionString = "server=containers-us-west-209.railway.app;port=6938;database=railway;user=root;password=5cu1Y8DVEYLMeej8yleH";
-        MySqlConnection connection = new MySqlConnection(connectionString);
-        MySqlCommand command = connection.CreateCommand();
-
-        command.CommandText = "SELECT COUNT(*) FROM Empregado WHERE Matricula = @Matricula";
-        command.Parameters.AddWithValue("@Matricula", user.Matricula);
-        connection.Open();
-        int count = Convert.ToInt32(command.ExecuteScalar());
-        connection.Close();
-
-        // Se a matrícula já existe, retornar um erro
-        if (count > 0)
+        [HttpPost(Name = "Adicionar Usuario")]
+        public IActionResult PostCadastrarEmpregado([FromBody] Empregado user)
         {
-            return BadRequest("Usuário já cadastrado");
-        }
-        if (string.IsNullOrEmpty(user.Email) || string.IsNullOrEmpty(user.Senha))
-        {
-            return BadRequest("Email e senha são obrigatórios");
-        }
+            string connectionString = "server=containers-us-west-209.railway.app;port=6938;database=railway;user=root;password=5cu1Y8DVEYLMeej8yleH";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand command = connection.CreateCommand();
 
-        command.CommandText = "INSERT INTO Empregado (Matricula, Nome, Funcao, Email, Senha, Resolutor, Setor_idSetor) VALUES (@Matricula, @Nome, @Funcao, @Email, @Senha, @Resolutor, @Setor_idSetor)";
-        command.Parameters.Clear();
-        command.Parameters.AddWithValue("@Matricula", user.Matricula);
-        command.Parameters.AddWithValue("@Nome", user.Nome);
-        command.Parameters.AddWithValue("@Funcao", user.Funcao);
-        command.Parameters.AddWithValue("@Email", user.Email);
-        command.Parameters.AddWithValue("@Senha", user.Senha);
-        command.Parameters.AddWithValue("@Resolutor", user.Resolutor);
-        command.Parameters.AddWithValue("@Setor_idSetor", user.Setor_idSetor);
+            command.CommandText = "SELECT COUNT(*) FROM Empregado WHERE Matricula = @Matricula";
+            command.Parameters.AddWithValue("@Matricula", user.Matricula);
+            connection.Open();
+            int count = Convert.ToInt32(command.ExecuteScalar());
+            connection.Close();
 
-        connection.Open();
-        command.ExecuteNonQuery();
-        connection.Close();
-        return Ok();
+            // Se a matrícula já existe, retornar um erro
+            if (count > 0)
+            {
+                return BadRequest("Usuário já cadastrado");
+            }
+
+            if (string.IsNullOrEmpty(user.Email) || string.IsNullOrEmpty(user.Senha))
+            {
+                return BadRequest("Email e senha são obrigatórios");
+            }
+
+            // Hashear a senha do usuário
+            string hashedPassword;
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(user.Senha));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                hashedPassword = builder.ToString();
+            }
+
+            command.CommandText = "INSERT INTO Empregado (Matricula, Nome, Funcao, Email, Senha, Resolutor, Setor_idSetor) VALUES (@Matricula, @Nome, @Funcao, @Email, @Senha, @Resolutor, @Setor_idSetor)";
+            command.Parameters.Clear();
+            command.Parameters.AddWithValue("@Matricula", user.Matricula);
+            command.Parameters.AddWithValue("@Nome", user.Nome);
+            command.Parameters.AddWithValue("@Funcao", user.Funcao);
+            command.Parameters.AddWithValue("@Email", user.Email);
+            command.Parameters.AddWithValue("@Senha", hashedPassword);
+            command.Parameters.AddWithValue("@Resolutor", user.Resolutor);
+            command.Parameters.AddWithValue("@Setor_idSetor", user.Setor_idSetor);
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+            return Ok();
+        }
     }
 }
